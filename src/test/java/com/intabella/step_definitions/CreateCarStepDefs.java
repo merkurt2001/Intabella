1d6b41a75eb1ddc456a7e4edc68e2bb253478a90
akinomer <ozsuakin@gmail.com>
2021-12-08 17:53:34 +0300

    2nd commit

--- src/test/java/com/intabella/step_definitions/CreateCarStepDefs.java
+++ src/test/java/com/intabella/step_definitions/CreateCarStepDefs.java
@@ -4,6 +4,7 @@ package com.intabella.step_definitions;
 import com.intabella.pages.DashboardPage;
 import com.intabella.pages.LoginPage;
 import com.intabella.utilities.BrowserUtils;
+import com.intabella.utilities.CarInfoReader;
 import com.intabella.utilities.ConfigurationReader;
 import com.intabella.utilities.Driver;
 import io.cucumber.java.en.Given;
@@ -18,7 +19,6 @@ import org.openqa.selenium.interactions.Actions;
 
 import java.io.File;
 import java.util.concurrent.TimeUnit;
-import com.intabella.utilities.ExcelUtil;
 
 import java.util.List;
 import java.util.Map;
@@ -68,28 +68,25 @@ public class CreateCarStepDefs {
 
     @And("^All form elements should be filled according to their types$")
     public void all_form_elements_should_be_filled_according_to_their_types(){
-        ExcelUtil crtCar = new ExcelUtil("src/test/resources/createCar.xls","crt");
-        List<Map<String, String>> dataList = crtCar.getDataList();
-        for (int i=0; i<1; i++) {
-            Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[LicensePlate]']")).sendKeys(dataList.get(i).get("License Plate"));
+            Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[LicensePlate]']")).sendKeys(CarInfoReader.get("licence_plate"));
             int count = Driver.get().findElements(By.xpath("//div[@data-ftid='custom_entity_type_Tags']/div")).size();
             for(int c=1; c <= count; c++){
                 Driver.get().findElement(By.xpath("(//div[@data-ftid='custom_entity_type_Tags']/div)["+c+"]/input")).click();
             }
-            Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[Driver]']")).sendKeys(dataList.get(i).get("driver"));
-            Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[Location]']")).sendKeys(dataList.get(i).get("Location"));
-            Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[ChassisNumber]']")).sendKeys(dataList.get(i).get("Chassis number").replace(".0",""));
-            Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[ModelYear]']")).sendKeys(dataList.get(i).get("Model year"));
+            Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[Driver]']")).sendKeys(CarInfoReader.get("driver_name"));
+            Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[Location]']")).sendKeys(CarInfoReader.get("location"));
+            Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[ChassisNumber]']")).sendKeys(CarInfoReader.get("chassis_number").replace(".0",""));
+            Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[ModelYear]']")).sendKeys(CarInfoReader.get("model_year"));
             System.out.println("Last otomater");
-            String lastOdometer = dataList.get(i).get("Last Odometer").replace(".0","");
+            String lastOdometer = CarInfoReader.get("last_odometer").replace(".0","");
             System.out.print(lastOdometer);
 
             Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[LastOdometer]']")).sendKeys(lastOdometer);
 
-            Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[CatalogValue]']")).sendKeys(dataList.get(i).get("Catalog Value"));
-            Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[SeatsNumber]']")).sendKeys(dataList.get(i).get("Seats Number"));
-            Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[DoorsNumber]']")).sendKeys(dataList.get(i).get("Doors Number"));
-            Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[Color]']")).sendKeys(dataList.get(i).get("Color"));
+            Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[CatalogValue]']")).sendKeys(CarInfoReader.get("catalog_value"));
+            Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[SeatsNumber]']")).sendKeys(CarInfoReader.get("seats_number"));
+            Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[DoorsNumber]']")).sendKeys(CarInfoReader.get("doors_number"));
+            Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[Color]']")).sendKeys(CarInfoReader.get("color"));
 
             Driver.get().findElement(By.xpath("(//span[text()='Choose a value...'])[1]")).click();
             Driver.get().findElement(By.xpath("//div[text()='Automatic']")).click();
@@ -97,19 +94,18 @@ public class CreateCarStepDefs {
             Driver.get().findElement(By.xpath("(//span[text()='Choose a value...'])[1]")).click();
             Driver.get().findElement(By.xpath("//div[text()='Hybrid']")).click();
 
-            Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[CO2Emissions]']")).sendKeys(dataList.get(i).get("CO2 Emissions"));
-            Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[Horsepower]']")).sendKeys(dataList.get(i).get("Horsepower").replace(".0",""));
-            Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[HorsepowerTaxation]']")).sendKeys(dataList.get(i).get("Horsepower Taxation"));
-            Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[Power]']")).sendKeys(dataList.get(i).get("Power").replace(".0",""));
+            Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[CO2Emissions]']")).sendKeys(CarInfoReader.get("co2_emissions"));
+            Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[Horsepower]']")).sendKeys(CarInfoReader.get("horsepower").replace(".0",""));
+            Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[HorsepowerTaxation]']")).sendKeys(CarInfoReader.get("horsepower_taxation"));
+            Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[Power]']")).sendKeys(CarInfoReader.get("power").replace(".0",""));
             BrowserUtils.scrollToElement(Driver.get().findElement(By.xpath("//input[contains(@name,'date_selector_custom_entity_type_ImmatriculationDate')]")));
-            Driver.get().findElement(By.xpath("//input[contains(@name,'date_selector_custom_entity_type_ImmatriculationDate')]")).sendKeys(dataList.get(i).get("Immatriculation Date"));
-            Driver.get().findElement(By.xpath("//input[contains(@name,'date_selector_custom_entity_type_FirstContractDate')]")).sendKeys(dataList.get(i).get("First Contract Date"));
+            Driver.get().findElement(By.xpath("//input[contains(@name,'date_selector_custom_entity_type_ImmatriculationDate')]")).sendKeys(CarInfoReader.get("immatriculation_date"));
+            Driver.get().findElement(By.xpath("//input[contains(@name,'date_selector_custom_entity_type_FirstContractDate')]")).sendKeys(CarInfoReader.get("first_contract_date"));
             File file = new File("src/test/resources/volvo.png");
             Driver.get().findElement(By.xpath("//input[@name='custom_entity_type[Logo][file]']")).sendKeys(file.getAbsolutePath());
             JavascriptExecutor js = (JavascriptExecutor) Driver.get();
             js.executeScript("document.getElementsByClassName('btn btn-success action-button')[0].click()");
             new DashboardPage().waitUntilLoaderScreenDisappear();
-        }
     }
 
 
